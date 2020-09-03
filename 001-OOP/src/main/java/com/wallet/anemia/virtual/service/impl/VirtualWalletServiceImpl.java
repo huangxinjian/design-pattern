bf4936a7674e07953e10db957483f7277d3133be
huangxinjian <924168702@qq.com>
2020-09-03 23:33:33 +0800

    添加初步代码

--- 001-OOP/src/main/java/com/wallet/anemia/virtual/service/impl/VirtualWalletServiceImpl.java
+++ 001-OOP/src/main/java/com/wallet/anemia/virtual/service/impl/VirtualWalletServiceImpl.java
@@ -1,9 +1,17 @@
 package com.wallet.anemia.virtual.service.impl;
 
+import com.wallet.anemia.virtual.repository.VirtualWalletRepository;
+import com.wallet.anemia.virtual.repository.VirtualWalletTransactionRepository;
 import com.wallet.anemia.virtual.service.VirtualWalletService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VirtualWalletServiceImpl implements VirtualWalletService {
 
+    @Autowired
+    private VirtualWalletRepository virtualWalletRepository;
+
+    @Autowired
+    private VirtualWalletTransactionRepository virtualWalletTransactionRepository;
 }
